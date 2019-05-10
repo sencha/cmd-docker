d222440dd674b17543fcfe9224ac0a738be0582a
Bryan Stone <aegixx@gmail.com>
2019-05-10 10:11:48 -0500

    Add major & minor tags
    Signed-off-by: Bryan Stone <aegixx@gmail.com>

--- Makefile
+++ Makefile
@@ -1,9 +1,10 @@
 CMD_VER := 6.6.0.13
-CMD_VER_SHORT := 6
+CMD_VER_MINOR := 6.6
+CMD_VER_MAJOR := 6
 
 IMG := imagehawk/cmd-docker
 
 build:
 	./build.sh
 
-export CMD_VER CMD_VER_SHORT IMG
+export CMD_VER CMD_VER_MAJOR CMD_VER_MINOR IMG
--- build.sh
+++ build.sh
@@ -3,10 +3,12 @@
 envsubst '${CMD_VER}' < Dockerfile.in > Dockerfile
 
 docker build --pull --tag ${IMG}:${CMD_VER} .
-docker tag ${IMG}:${CMD_VER} ${IMG}:${CMD_VER_SHORT}
+docker tag ${IMG}:${CMD_VER} ${IMG}:${CMD_VER_MAJOR}
+docker tag ${IMG}:${CMD_VER} ${IMG}:${CMD_VER_MINOR}
 docker tag ${IMG}:${CMD_VER} ${IMG}:latest
 
 echo "To push latest docker images:"
 echo "    docker push ${IMG}:${CMD_VER}"
-echo "    docker push ${IMG}:${CMD_VER_SHORT}"
+echo "    docker push ${IMG}:${CMD_VER_MINOR}"
+echo "    docker push ${IMG}:${CMD_VER_MAJOR}"
 echo "    docker push ${IMG}:latest"
